4655d462379a86a84d4586df03f6b67dd1b920ec
Gourav <gouravgunaga555@gmail.com>
2024-02-27 15:47:03 +0530

    Updated the banner Component to provide the error img

--- src/components/banner..js
+++ src/components/banner..js
@@ -1,5 +1,42 @@
-export const Banner = () => {
+import { useState } from "react";
+import erroImg from "./Errorimg.png";
+
+const ErrorImg = () => {
     return (
-        <h1>Banner</h1>
-    )
-}
+        <>
+            <p>Country of the Day: India</p>
+            <div className="countryOftheDay">
+                <img src={erroImg} alt="Error Img" width="500px" />
+                <p>Get to know more about India </p>
+            </div>
+        </>
+    );
+};
+
+const FlagImg = ({ setImgErr }) => {
+    return (
+        <>
+            <p>Country of the Day: India</p>
+            <img
+                src="https://flagcdn.com/w640/in.png"
+                alt="Map of the country"
+                onError={() => setImgErr(true)}
+                width="400px"
+            />
+            <p>Get to know more about India </p>
+        </>
+    );
+};
+
+export const Banner = () => {
+    const [imgError, setImgErr] = useState(false);
+    return (
+        <div>
+            {imgError ? (
+                <ErrorImg />
+            ) : (
+                <FlagImg setImgErr={setImgErr} />
+            )}
+        </div>
+    );
+};
