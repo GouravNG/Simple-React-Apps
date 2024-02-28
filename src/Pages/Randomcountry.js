24fb774c6ec22b2a04d2f03a84d9ca005c6fbd1d
Gourav <gouravgunaga555@gmail.com>
2024-02-28 11:56:22 +0530

    Random country page layout is completed

---
+++ src/Pages/Randomcountry.js
@@ -0,0 +1,36 @@
+export const RandomCountry = () => {
+    return (
+        <div className="container">
+            <div className="upperBox">
+                <div className="mapBox">
+                    <div className="mapImg">
+                        Flag
+                    </div>
+                    <div className="mainText">
+                        <p>India</p>
+                        <p>Population</p>
+                    </div>
+                </div>
+                <div className="geoBox">GeoMap</div>
+            </div>
+            <div className="lowerBox">
+                <div className="left">
+                    <ul>
+                        <li>One</li>
+                        <li>two</li>
+                        <li>three</li>
+                        <li>Four</li>
+                    </ul>
+                </div>
+                <div className="right">
+                    <ul>
+                        <li>Five</li>
+                        <li>six</li>
+                        <li>seven</li>
+                        <li>eight</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
